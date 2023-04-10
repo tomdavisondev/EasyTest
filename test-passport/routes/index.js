99fa59813efbc9eeed4219f37fa97b4e22747750
Tom <tomwilliamdavison@gmail.com>
2023-04-10 13:52:50 +1000

    Updated index js get requests to used cached changes
    Cached changes should be updated if they are either not already included in the cache or if the server detects changes

--- test-passport/routes/index.js
+++ test-passport/routes/index.js
@@ -2,111 +2,174 @@ const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated } = require('../config/auth');
 
+const NodeCache = require( "node-cache" );
+const cache = new NodeCache();
+
 const Project = require('../models/Project');
 const Requirements = require('../models/Requirements');
 
-router.get('/project/:projectname', ensureAuthenticated, (req, res) => {
-	Requirements.find({}).exec(function(err, requirements) {
-	Project.find({}).exec(function(err, projects) {
-		Project.findOne({projectname: req.params.projectname})
-			.then(project => {
-				if (project) {
-					res.render('project', {
-						req: req,
-						name: req.user.name,
-						project: project,
-						testcases: project.testcases,
-						teststeps: project.testcases.teststeps,
-						projects: projects,
-						requirements: requirements
-					})
-				} else {
-					//TODO: Proper validation when a project is not found
-					// this shouldn't ever happen but eh, worth doing
-					console.log("Error: project not found error");
-					console.log(req.projectName);
-					console.log(req.params);
-				}
-			});
-	});
-});
-});
+Requirements.watch().on('change', async () => {
+	console.log('Requirement collection has changed, invalidating cache...');
+	await cache.del('requirements');
+  });
 
-router.get('/requirements/:requirementname', ensureAuthenticated, (req, res) => {
-	Project.find({}).exec(function(err, projects) {
-	Requirements.find({}).exec(function(err, requirements) {
-		Requirements.findOne({requirementname: req.params.requirementname})
-			.then(requirement => {
-				if (requirement) {
-					res.render('requirement', {
-						req: req,
-						name: req.user.name,
-						requirement: requirement,
-						requirementid: requirement.requirementid,
-						requirements: requirements,
-						projects: projects
-					})
-				} else {
-					//TODO: Proper validation when a project is not found
-					// this shouldn't ever happen but eh, worth doing
-					console.log("Error: could not load " + requirementname);
-					console.log(req.requirementname);
-					console.log(req.params);
-				}
-			});
-	});
-});
-});
+Project.watch().on('change', async () => {
+	console.log('Project collection has changed, invalidating cache...');
+	await cache.del('projects');
+  });
 
-router.get('/project/:projectname/:testcasename', ensureAuthenticated, (req, res) => {
-	Requirements.find({}).exec(function(err, requirements){
-	Project.find({}).exec(function(err, projects) {
-		Project.findOne({projectname: req.params.projectname})
-			.then(project => {
-				if (project) {
-					var testcase = project.testcases.find(obj => {
-						return obj.name === req.params.testcasename
-					});
-					//console.log(testcase);
-					res.render('testcase', {
-						req: req,
-						name: req.user.name,
-						project: project,
-						testcase,
-						testcases: project.testcases,
-						testcasename: req.params.testcasename,
-						projectname: project.projectname,
-						teststeps: project.testcases.teststeps,
-						projects: projects,
-						requirements: requirements,
-					})
-				} else {
-					//TODO: Proper validation when a project is not found
-					// this shouldn't ever happen but eh, worth doing
-					console.log("Error: project not found error");
-					console.log(req.projectName);
-					console.log(req.params);
-				}
-			});
-	});
-});
-});
+router.get('/project/:projectname', ensureAuthenticated, async (req, res) => {
+	try {
+		let requirements = await cache.get("requirements");
+		let projects = await cache.get("projects");
 
-// Dashboard
-router.get('/dashboard', ensureAuthenticated, (req, res) =>
-	Requirements.find({}).exec(function(err, requirements){
-		Project.find({}).exec(function(err, projects) {
-			if (err) throw err;
-			res.render('dashboard', {
+		if (requirements == undefined) {
+			requirements = await Requirements.find({}).exec();
+			await cache.set("requirements", requirements);
+		}
+
+		if (projects == undefined) {
+			projects = await Project.find({}).exec();
+			await cache.set("projects", projects);
+		}
+
+		let project = await cache.get(`project-${req.params.projectname}`);
+		if (!project) {
+			project = await Project.findOne({ projectname: req.params.projectname }).exec();
+			if (project) {
+				await cache.set(`project-${req.params.projectname}`, project);
+			}
+		}
+
+		if (project) {
+			res.render('project', {
+				req: req,
 				name: req.user.name,
+				project: project,
+				testcases: project.testcases,
+				teststeps: project.testcases.teststeps,
 				projects: projects,
-				requirements: requirements,
-				req: req
-			});
-		});
-	})
-);
+				requirements: requirements
+			})
+		} else {
+			//TODO: Proper validation when a project is not found
+			// this shouldn't ever happen but eh, worth doing
+			console.log("Error: project not found error");
+			console.log(req.projectName);
+			console.log(req.params);
+		}
+	} catch (error) {
+		console.log(error);
+	}
+});
 
+router.get('/requirements/:requirementname', ensureAuthenticated, async (req, res) => {
+	try {
+	const projects = await cache.get("projects");
+	const requirements = await cache.get("requirements");
+	
+	if (!requirements) {
+		const requirements = await Requirements.find({}).exec();
+		await cache.set("requirements", requirements);
+	}
+
+	if (!projects) {
+		const projects = await Project.find({}).exec();
+		await cache.set("projects", projects);
+	}
+
+	const requirement = await Requirements.findOne({requirementname: req.params.requirementname});
+
+	if (requirement) {
+		res.render('requirement', {
+			req: req,
+			name: req.user.name,
+			requirement: requirement,
+			requirementid: requirement.requirementid,
+			requirements: requirements,
+			projects: projects
+		});
+	} else {
+		//TODO: Proper validation when a requirement is not found
+		// this shouldn't ever happen but worth doing
+		console.log("Error: could not load " + req.params.requirementname);
+	}
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+
+router.get('/project/:projectname/:testcasename', ensureAuthenticated, async (req, res) => {
+	try {
+	const requirements = await cache.get("requirements");
+	const projects = await cache.get("projects");
+
+	if (!requirements) {
+		const requirements = await Requirements.find({}).exec();
+		await cache.set("requirements", requirements);
+	}
+
+	if (!projects) {
+		const projects = await Project.find({}).exec();
+		await cache.set("projects", projects);
+	}
+
+	const project = await Project.findOne({projectname: req.params.projectname});
+
+	if (project) {
+		const testcase = project.testcases.find(obj => {
+			return obj.name === req.params.testcasename;
+		});
+
+		res.render('testcase', {
+			req: req,
+			name: req.user.name,
+			project: project,
+			testcase,
+			testcases: project.testcases,
+			testcasename: req.params.testcasename,
+			projectname: project.projectname,
+			teststeps: testcase.teststeps,
+			projects: projects,
+			requirements: requirements,
+		});
+	} else {
+		//TODO: Proper validation when a project is not found
+		// this shouldn't ever happen but worth doing
+		console.log("Error: project not found error");
+		console.log(req.params);
+	}
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+router.get('/dashboard', ensureAuthenticated, async (req, res) => {
+	try {
+		let requirements = await cache.get("requirements");
+		let projects = await cache.get("projects");
+
+		if (requirements == undefined) {
+			requirements = await Requirements.find({}).exec();
+			await cache.set("requirements", requirements);
+		}
+
+		if (projects == undefined) {
+			projects = await Project.find({}).exec();
+			await cache.set("projects", projects);
+		}
+
+		res.render('dashboard', {
+			name: req.user.name,
+			projects: projects,
+			requirements: requirements,
+			req: req
+		});
+	} catch (error) {
+		console.log(error);
+	}
+});
 
 router.get('/', function(req, res) {
 	res.redirect('/users/login');
